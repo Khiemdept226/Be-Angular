2894a4d451a58e625a2189d4e51def6f8a164a51
HuuKhiem <huukhiem001@gmail.com>
2023-06-12 14:19:48 +0700

    first commit

---
+++ src/main/java/com/example/demo/Entity/User.java
@@ -0,0 +1,24 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer Id;
+    private String name;
+    private String email;
+    private String phone;
+    private String avatar;
+    private String password;
+}
